c86cc298a1ef85532462943af8f55e9aaa1127b7
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-10-20 14:31:44 -0700

    updated routes for various pages

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,10 +1,19 @@
-import React, { Fragment } from 'react';
-import { Redirect, Route, Switch } from 'react-router';
+import React, { Fragment } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { MenuBar } from "../components";
+import { Home, Items, Share, Profile } from "../pages";
 
 export default () => (
   <Fragment>
     {/* @TODO: Add your menu component here */}
+    <MenuBar />
     <Switch>
+      <Route exact path="/welcome" component={Home} />
+      <Route path="/share" component={Share} />
+      <Route path="/items" component={Items} />
+      <Route path="/profile/:id" component={Profile} />
+      <Route path="/profile" component={Profile} />
+      <Redirect from="*" to="/items" />
       {/**
        * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
        *
@@ -12,6 +21,7 @@ export default () => (
        *
        * Later, we'll add logic to send users to one set of routes if they're logged in,
        * or only view the /welcome page if they are not.
+       *
        */}
     </Switch>
   </Fragment>
