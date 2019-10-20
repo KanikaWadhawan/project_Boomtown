f07080476ee984131ae21677c9633fbcaeff6448
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-10-20 14:26:09 -0700

    Updated querries and mutations for client-server

--- client/src/apollo/queries.js
+++ client/src/apollo/queries.js
@@ -6,81 +6,114 @@ import gql from "graphql-tag";
 
 const ItemFields = gql`
   fragment ItemFields on Item {
-    # @TODO: Create a fragment to query the following fields for an item:
-    #
-    # id
-    # title
-    # imageurl
-    # description
-    # created
-    # tags (id and title fields)
-    # itemowner (id, fullname, email, and bio fields)
-    # borrower (id, fullname, email, and bio fields)
-    #
-    # See the Apollo docs for instructions on how to use fragments:
-    # https://www.apollographql.com/docs/angular/features/fragments.html
+    id
+    title
+    imageurl
+    description
+    created
+    tags {
+      id
+      title
+    }
+    itemowner {
+      id
+      email
+      fullname
+      bio
+    }
+    borrower {
+      id
+      email
+      fullname
+      bio
+    }
   }
 `;
-export const ITEM_QUERY = gql`
-  query item($id: ID!) {
-    # @TODO: Query an item by its id and return the ItemFields fragment.
-  }
-  ${ItemFields}
-`;
+
+// export const ITEM_QUERY = gql`
+//   query item($id: ID!) {
+//     # @TODO: Query an item by its id and return the ItemFields fragment.
+//   }
+//   ${ItemFields}
+// `;
 
 export const ALL_ITEMS_QUERY = gql`
   query items($filter: ID) {
-    # @TODO: Query items (optionally by tag id) and return the ItemFields fragment.
+    items(filter: $filter) {
+      ...ItemFields
+    }
   }
   ${ItemFields}
 `;
 
 export const ALL_USER_ITEMS_QUERY = gql`
   query user($id: ID!) {
-    # @TODO: Query the bio, email, fullname, items, and borrowed for the user by id
-    # Use the ItemFields fragment for the items and borrowed fields.
+    bio
+    email
+    fullname
+    items {
+      ...ItemFields
+    }
+    borrowed {
+      ...ItemFields
+    }
   }
   ${ItemFields}
 `;
 
 export const ALL_TAGS_QUERY = gql`
   query {
-    # @TODO: Query the id and title fields for tags.
+    tags {
+      id
+      title
+    }
   }
 `;
 
+//# @TODO: Pass the item and image into the addItem mutation as arguments
+//# and return the new item id when the mutation is complete.
 export const ADD_ITEM_MUTATION = gql`
-  mutation addItem($item: NewItemInput!) {
-    # @TODO: Pass the item and image into the addItem mutation as arguments
-    # and return the new item id when the mutation is complete.
+  mutation addNewItem(
+    $title: String!
+    $description: String!
+    $tags: [AssignedTag]!
+  ) {
+    addItem(input: { title: $title, description: $description, tags: $tags }) {
+      title
+      description
+      tags {
+        id
+        title
+      }
+    }
   }
 `;
 
-/**
- * Auth-related queries and mutations.
- */
+// /**
+//  * Auth-related queries and mutations.
+//  */
 
-export const VIEWER_QUERY = gql`
-  query {
-    # @TODO: Query the id, email, fullname, and bio fields for the viewer.
-  }
-`;
-export const LOGOUT_MUTATION = gql`
-  mutation {
-    # @TODO: Run the logout mutation.
-  }
-`;
+// export const VIEWER_QUERY = gql`
+//   query {
+//     # @TODO: Query the id, email, fullname, and bio fields for the viewer.
+//   }
+// `;
+// export const LOGOUT_MUTATION = gql`
+//   mutation {
+//     # @TODO: Run the logout mutation.
+//   }
+// `;
 
-export const SIGNUP_MUTATION = gql`
-  mutation signup($user: SignupInput!) {
-    # @TODO: Pass the user into the signup mutation as an argument
-    # and return the token and user id.
-  }
-`;
+// export const SIGNUP_MUTATION = gql`
+//   mutation signup($user: SignupInput!) {
+//     # @TODO: Pass the user into the signup mutation as an argument
+//     # and return the token and user id.
+//   }
+// `;
 
-export const LOGIN_MUTATION = gql`
-  mutation login($user: LoginInput!) {
-    # @TODO: Pass the user into the login mutation as an argument
-    # and return the token and user id.
-  }
-`;
+// export const LOGIN_MUTATION = gql`
+//   mutation login($user: LoginInput!) {
+//     # @TODO: Pass the user into the login mutation as an argument
+//     # and return the token and user id.
+//   }
+// `;
