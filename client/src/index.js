319b66943ad626f3fa18d9edda60040bb5fc27f6
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-10-20 14:52:07 -0700

    added link to material-ui-icons

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 // @TODO: Uncomment each module as needed in your client app
-// import { ApolloProvider } from 'react-apollo'
-// import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from "react-apollo";
+
 // import { Provider as ReduxProvider } from 'react-redux'
 // -------------------------------
 
-import registerServiceWorker from './registerServiceWorker';
-import theme from './theme';
+import registerServiceWorker from "./registerServiceWorker";
+import theme from "./theme";
+import client from "./apollo";
 
 /**
  * @TODO: Initialize Apollo Client
@@ -33,6 +34,8 @@ import theme from './theme';
  * Below in your <App />, nest your <AppRoutes /> inside of <BrowserRouter />
  * component to enable routing in your client app.
  */
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes";
 
 /**
  * @TODO: Initialize Redux Store
@@ -57,19 +60,23 @@ import theme from './theme';
  */
 
 // @TODO: Remove this import once you have your router working below
-import Home from './pages/Home';
+import Home from "./pages/Home";
 // -------------------------------
 
-import './index.css';
+import "./index.css";
 
 const App = () => {
   return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <Home />
-    </MuiThemeProvider>
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Routes></Routes>
+        </Router>
+      </MuiThemeProvider>
+    </ApolloProvider>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
