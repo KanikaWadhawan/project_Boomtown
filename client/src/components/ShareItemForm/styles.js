410e08fedb956d5ddce0512972a386afa4fc9ee7
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-10-21 20:41:14 -0700

    Worked on the ShareItemForm Component of the Share Page

--- client/src/components/ShareItemForm/styles.js
+++ client/src/components/ShareItemForm/styles.js
@@ -1,3 +1,19 @@
-const styles = theme => ({});
+const styles = theme => ({
+  shareTitleQuote: {
+    fontWeight: 600
+  },
+  ShareItemFormContents: {
+    marginBottom: theme.spacing(4)
+  },
+  ShareItemFormGroup: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr) )",
+    gridColumnGap: theme.spacing(2),
+    gridRowGap: theme.spacing(1)
+  },
+  ShareItemFormTags: {
+    marginRight: 0
+  }
+});
 
 export default styles;
