cea3fe514063e855888ec422a3d71eb7c3eab856
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-10-22 12:57:59 -0700

    added seperate component for ShareItemPreview

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,4 @@ export { default as ShareItemForm } from "./ShareItemForm";
 export { default as MenuBarIcon } from "./MenuBarIcon";
 export { default as ItemsGrid } from "./ItemsGrid";
 export { default as ItemCard } from "./ItemCard";
+export { default as ShareItemPreview } from "./ShareItemPreview";
